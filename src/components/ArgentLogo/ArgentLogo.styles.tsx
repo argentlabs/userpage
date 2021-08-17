9935aa40ced2b4f7a58623cac3fc166f19994702
Janek Rahrt <janek.rahrt@me.com>
2021-08-17 09:18:08 +0200

    allow mac 13"

--- src/components/ArgentLogo/ArgentLogo.styles.tsx
+++ src/components/ArgentLogo/ArgentLogo.styles.tsx
@@ -3,15 +3,7 @@ import styled from "styled-components"
 import ArgentLogoSvg from "../Svgs/ArgentLogo"
 
 export const ArgentLogo = styled(ArgentLogoSvg)`
-  height: 42px;
-
-  @media only screen and (min-height: 666px) {
-    height: 44px;
-  }
-
-  @media only screen and (min-width: 480px) {
-    height: 48px;
-  }
+  height: clamp(32px, 5vh, 48px);
 `
 
 export const Anchor = styled.a`
--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -5,21 +5,8 @@ import { centerMixin } from "../../mixins.style"
 export const PageWrapper = styled.div`
   ${centerMixin}
 
-  padding: 2rem 0;
+  padding: clamp(2rem, 8vh, 6rem) 0;
   > a {
-    margin-bottom: 2rem;
-  }
-
-  @media only screen and (min-height: 666px) {
-    > a {
-      margin-bottom: 3rem;
-    }
-  }
-
-  @media only screen and (min-width: 480px) {
-    padding: 6rem 0;
-    > a {
-      margin-bottom: 5rem;
-    }
+    margin-bottom: clamp(2rem, 8vh, 5rem);
   }
 `
