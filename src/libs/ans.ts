34be28c55d30d9173c4f9afab14108808ece0855
Janek Rahrt <janek.rahrt@me.com>
2021-12-14 10:30:28 +0100

    feat: always zksync view
    move address view to sent screen

--- src/libs/ans.ts
+++ src/libs/ans.ts
@@ -3,7 +3,8 @@ import chunk from "lodash.chunk"
 import joinUrl from "url-join"
 
 import { readProvider } from "./web3"
-import { AccountResult, fetchAccount } from "./zksyncApi"
+
+// import { AccountResult, fetchAccount } from "./zksyncApi"
 
 const {
   REACT_APP_ARGENT_API_ANS_WALLET_ENDPOINT,
@@ -69,9 +70,9 @@ export const getAddressFromEns = (name: string) =>
   readProvider.resolveName(name)
 const resultOrNull = <T extends any>(promise: Promise<T>): Promise<T | null> =>
   promise.catch(() => null)
-const zkAccountExists = (zkAccount: AccountResult | null): boolean =>
-  Boolean(zkAccount) &&
-  zkAccount!.pubKeyHash !== "sync:0000000000000000000000000000000000000000"
+// const zkAccountExists = (zkAccount: AccountResult | null): boolean =>
+//   Boolean(zkAccount) &&
+//   zkAccount!.pubKeyHash !== "sync:0000000000000000000000000000000000000000"
 
 /**
  * Resolves a given name to its address and additional info about the wallet
@@ -119,13 +120,16 @@ export const getUserInfo = async (name: string): Promise<Ans> => {
     throw new Error("Not found")
   }
 
-  // double check zkSync
-  if (!response.hasZkSync) {
-    const zkAccount = await resultOrNull(fetchAccount(response.walletAddress))
-    if (zkAccountExists(zkAccount)) {
-      response.hasZkSync = true
-    }
-  }
+  // always show zkSync status
+  response.hasZkSync = true
+
+  // // double check zkSync
+  // if (!response.hasZkSync) {
+  //   const zkAccount = await resultOrNull(fetchAccount(response.walletAddress))
+  //   if (zkAccountExists(zkAccount)) {
+  //     response.hasZkSync = true
+  //   }
+  // }
 
   return response
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,6 @@ import { Helmet } from "react-helmet"
 
 import ArgentLogo from "../../components/ArgentLogo"
 import Avatar from "../../components/Avatar"
-import { SecondaryButtonWithIcon } from "../../components/Button/Button"
 import Center from "../../components/Center"
 import DarkmodeSwitch from "../../components/DarkmodeSwitch"
 import IconButton from "../../components/IconButton"
@@ -11,9 +10,8 @@ import PageWrapper from "../../components/PageWrapper"
 import Box from "../../components/ProfileBox"
 import Add from "../../components/Svgs/Add"
 import Gallery from "../../components/Svgs/Gallery"
-import Qr from "../../components/Svgs/Qr"
 import { useRouterMachine } from "../../states/hooks"
-import { IconBar, SAddress } from "./Home.style"
+import { IconBar } from "./Home.style"
 
 export const HomePage: FC = () => {
   const [
@@ -32,17 +30,7 @@ export const HomePage: FC = () => {
       <ArgentLogo />
       <Center>
         <Avatar pubkey={walletAddress} />
-        <Box title={`@${name}`}>
-          <SAddress
-            confirmationText="Copied!"
-            address={walletAddress}
-            short
-            zkSync={hasZkSync}
-          />
-          <SecondaryButtonWithIcon onClick={() => send("PUSH_VAULT")}>
-            <Qr /> View address
-          </SecondaryButtonWithIcon>
-        </Box>
+        <Box title={`@${name}`} />
         <IconBar direction="row" gap="40px">
           <IconButton
             Icon={<Add />}
